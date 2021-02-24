7aa7526506a5bb8624b7bc4518c3de0cfb661796
liuhuan <ares1573@163.com>
2021-02-24 20:00:47 +0800

    fix

---
+++ src/main/java/com/imust/entity/Emp.java
@@ -0,0 +1,12 @@
+package com.imust.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Emp {
+    private Dept dept;
+}
